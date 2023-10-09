b5b4004e6e746c903ab141ed6341731b91314d54
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-10-09 11:21:48 -0400

    Canines and chiropterans now drink water

--- models/Canine.php
+++ models/Canine.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace app\models;
-
-class Canine implements Mammal
+/**
+ * Canines are mammals that drink water, but that doesn't mean they aren't
+ * mammals anymore and MammalThatDrinksWater extends Mammal
+ * Therefore Canines are still a mammal(Liskov)
+ */
+class Canine implements MammalThatDrinksWater
 {
     public function move(): string
     {
         return "Canines walk.";
+    }
+
+    public function drinkWater(): string
+    {
+        return "This canine drinks water";
     }
 }
--- models/Chiropteran.php
+++ models/Chiropteran.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace app\models;
-
-class Chiropteran implements Mammal
+/**
+ * Quiropterans are mammals that drink water, but that doesn't mean they aren't
+ * mammals anymore and MammalThatDrinksWater extends Mammal
+ * Therefore Quiropterans are still a mammal(Liskov)
+ */
+class Chiropteran implements MammalThatDrinksWater
 {
-    public function move (): string
+    public function move(): string
     {
         return "Bats are the only mammal that flies.";
+    }
+
+    public function drinkWater(): string
+    {
+        return "This Chiropteran drinks water";
     }
 }
