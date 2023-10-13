cd6c3704b6951d6558974e4b94430b72c2461e69
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-10-13 16:44:21 -0400

    Getters and setters added to ZooAnimal.php

--- classes/ZooAnimal.php
+++ classes/ZooAnimal.php
@@ -20,13 +20,44 @@ abstract class ZooAnimal
         return $this->foodInfo;
     }
 
-    public function getExtraInfo()
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getBreed(): string
+    {
+        return $this->breed;
+    }
+
+    public function setBreed($breed): void
+    {
+        $this->breed = $breed;
+    }
+
+    /**
+     * @return string returns each element in ```$this->extrainfo``` as string separated by a ```<br>``` tag
+     */
+    public function getExtraInfo(): string
     {
         $keys = array_keys($this->extraInfo);
         return array_reduce($keys, function ($carry, $key){
             $item = $this->extraInfo[$key];
             return "$carry$key: $item<br>";
         }, "");
+    }
+
+    /**
+     * @return array returns ```$this->extrainfo``` as an associative array
+     */
+    public function getExtraInfoAsArray(): array
+    {
+        return $this->extraInfo;
     }
 
     public function getAllInfo(): string
