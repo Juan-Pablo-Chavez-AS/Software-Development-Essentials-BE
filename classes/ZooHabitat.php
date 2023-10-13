7a14b54d3ba0627feb5137654e1719a1a885137e
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-10-13 18:08:51 -0400

    WIP: ZooHabitat class

---
+++ classes/ZooHabitat.php
@@ -0,0 +1,47 @@
+<?php
+
+class ZooHabitat
+{
+    protected array $animals;
+    protected string $name;
+    protected string $animalClassification;
+
+    /**
+     * @param string $classification Should be the value of ```::class``` from a concrete class that implements ```ZooAnimal```
+     */
+    public function __construct(string $classification, $name)
+    {
+        $this->animals = array();
+        $this->animalClassification = $classification;
+        $this->name = $name;
+    }
+
+    public function addAnimal(ZooAnimal $animal): void
+    {
+        $animalClass = get_class($animal);
+        if($this->animalClassification !== $animalClass) {
+            throw new InvalidArgumentException("This habitat only accepts $this->animalClassification, tried to add an $animalClass");
+        }
+        $animal[] = $animal;
+    }
+
+    /**
+     * @param array $animals Zoo Animals that don't correspond to this habitat are filtered before
+     * trying to add them to the habitat
+     */
+    public function addAnimals(array $animals): void
+    {
+        $filteredAnimals = array_filter($animals, function($animal) {
+            $animalClass = get_class($animal);
+            return $this->animalClassification === $animalClass;
+        });
+        array_push($this->animals, ...$filteredAnimals);
+    }
+
+    public function habitatAnimalsInfo(): string
+    {
+        $habitatInfo = "<h4>$this->name</h4>";
+        // WIP: Think of a string output format for this class, something for the dom
+        return $habitatInfo;
+    }
+}
