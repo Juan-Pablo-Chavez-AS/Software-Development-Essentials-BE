b8384da7ca28ce83dd211fc5c72fddbe3cbb8d72
Juan Pablo Chavez Arteaga <juanpablo.chavez@training.assuresoft.com>
2023-10-13 17:42:56 -0400

    All dogs now have extraInfo in index.php

--- index.php
+++ index.php
@@ -5,16 +5,19 @@
     $dogFoodInfo = new FoodInfo("Carnivore");
 
     $dog1ExtraInfo = [
-        "Found" => "He was lost in a woodshop",
+        "Found" => "He was found in a woodshop",
     ];
-    $dog1ExtraInfo = [
-        "Found" => "He was lost in a woodshop",
+    $dog2ExtraInfo = [
+        "Origin" => "Puddles were made for fancy millionares",
+    ];
+    $dog3ExtraInfo = [
+        "Gender" => "Hasn't decided yet",
+        "AKA" => "Big Boss",
     ];
 
     $dog1 = new Canine("Husky", "Pinocchio", $dogFoodInfo, $dog1ExtraInfo);
-    $dog2 = new Canine("Puddle", "Cloudy", $dogFoodInfo);
-    $dog3 = new Canine("Golden Retriever", "Boss", $dogFoodInfo);
-
+    $dog2 = new Canine("Puddle", "Cloudy", $dogFoodInfo, $dog2ExtraInfo);
+    $dog3 = new Canine("Golden Retriever", "Boss", $dogFoodInfo, $dog3ExtraInfo);
     $dogs = [
         $dog1,
         $dog2,
@@ -25,5 +28,4 @@
         echo $dog->getAllInfo();
         echo "<hr>";
     });
-
 ?>
